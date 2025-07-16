bdd2c7ec32c8457506499d8379cdbf03641edb5e
HAROLD <haroldmediana2010@gmail.com>
2025-07-16 10:03:37 -0500

    Update Owner•join.js

--- plugins/Owner•join.js
+++ plugins/Owner•join.js
@@ -2,12 +2,12 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🩵 Ingresa el enlace del Grupo.`)
+if (!text) return m.reply(`🧡 Ingresa el enlace del Grupo.`)
 try {
 let [_, code] = text.match(linkRegex) || []
 if (!code) return m.reply('📣 Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
-m.reply(`🩵 Me uní al grupo con éxito...`)
+m.reply(`🏐 Me uní al grupo con éxito...`)
 } catch {
 return m.reply('✘ Ocurrió un error al unirme al grupo...')}}
 
