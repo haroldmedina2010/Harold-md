22dc4522eaa8e1d33e09ac96c765106167bcc343
HAROLD <haroldmediana2010@gmail.com>
2025-08-05 07:54:17 -0500

    Update Game•formarpareja5.js

--- plugins/Game•formarpareja5.js
+++ plugins/Game•formarpareja5.js
@@ -1,62 +1,43 @@
 let R = Math.random;
 let Fl = Math.floor;
 let toM = (a) => "@" + a.split("@")[0];
-function handler(m, {groupMetadata}) {
-  let ps = groupMetadata.participants.map((v) => v.id);
-  let a = ps[Fl(R() * ps.length)];
-  let b;
-  do b = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let c;
-  do c = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let d;
-  do d = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let e;
-  do e = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let f;
-  do f = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let g;
-  do g = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let h;
-  do h = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let i;
-  do i = ps[Fl(R() * ps.length)];
-  while (b === a);
-  let j;
-  do j = ps[Fl(R() * ps.length)];
-  while (b === a);
+
+function getRandomUnique(arr, count) {
+  const selected = new Set();
+  while (selected.size < count && selected.size < arr.length) {
+    const rand = Fl(R() * arr.length);
+    selected.add(arr[rand]);
+  }
+  return [...selected];
+}
+
+async function handler(m, { groupMetadata }) {
+  const ps = groupMetadata.participants.map((v) => v.id);
+
+  if (ps.length < 10) {
+    return m.reply("🚫 El grupo necesita al menos 10 participantes para formar 5 parejas.");
+  }
+
+  const [a, b, c, d, e, f, g, h, i, j] = getRandomUnique(ps, 10);
+
   m.reply(
-    `*_😍Las 5 mejores parejas del grupo😍_*
-    
-*_1.- ${toM(a)} y ${toM(b)}_*
-- Esta pareja esta destinada a estar junta 💙
-
-*_2.- ${toM(c)} y ${toM(d)}_*
-- Esta pareja son dos pequeños tortolitos enamorados ✨
-
-*_3.- ${toM(e)} y ${toM(f)}_*
-- Ufff y que decir de esta pareja, ya hasta familia deberian tener 🤱🧑‍🍼
-
-*_4.- ${toM(g)} y ${toM(h)}_*
-- Estos ya se casaron en secreto 💍
-
-*_5.- ${toM(i)} y ${toM(j)}_*
-- Esta pareja se esta de luna de miel ✨🥵😍❤️*`,
+    `*_😍Las 5 mejores parejas del grupo😍_*\n\n` +
+    `*_1.- ${toM(a)} y ${toM(b)}_*\n- Esta pareja está destinada a estar junta 💙\n\n` +
+    `*_2.- ${toM(c)} y ${toM(d)}_*\n- Son dos pequeños tortolitos enamorados ✨\n\n` +
+    `*_3.- ${toM(e)} y ${toM(f)}_*\n- Ufff, ya hasta familia deberían tener 🤱🧑‍🍼\n\n` +
+    `*_4.- ${toM(g)} y ${toM(h)}_*\n- Estos ya se casaron en secreto 💍\n\n` +
+    `*_5.- ${toM(i)} y ${toM(j)}_*\n- Esta pareja está de luna de miel ✨🥵😍❤️*`,
     null,
     {
       mentions: [a, b, c, d, e, f, g, h, i, j],
     }
   );
 }
+
 handler.help = ["formarpareja5"];
 handler.tags = ["main", "fun"];
 handler.command = ["formarpareja5"];
 handler.register = true;
 handler.group = true;
+
 export default handler;
